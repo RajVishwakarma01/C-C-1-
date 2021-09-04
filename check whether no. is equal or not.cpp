8b90793cb807168ea2773427923773e8414f2e7e
Raj Vishwakarma <87299771+RajVishwakarma01@users.noreply.github.com>
2021-09-04 14:17:16 +0100

    Program for input two number and check whether it is Equal or not.

---
+++ check whether no. is equal or not.cpp
@@ -0,0 +1,19 @@
+//“Program for input two number and check whether it is Equal or not”.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter any two number : ";
+	cin>>a>>b;
+	cout<<endl;
+	if(a==b)
+	{
+		cout<<" Number's are equal ";
+	}
+	else
+	{
+		cout<<" Number's are not equal ";
+	}
+	return 0;
+}
