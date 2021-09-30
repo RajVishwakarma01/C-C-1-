f7fb6ea4ee8914d5f577334f84d8c3aac8117be0
Raj Vishwakarma <87299771+RajVishwakarma01@users.noreply.github.com>
2021-09-30 16:58:10 +0530

    A Program to find the sum of the first 100 natural numbers

---
+++ sum of the first 100 natural numbers.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+    using namespace std;
+    int main()
+    {
+        
+       int sum , N=100; 
+        
+        sum= N*(N+1)/2; 
+        cout<<"The Sum of first "<< N <<" Natural Numbers is "<< sum; 
+        return 0;
+    }
